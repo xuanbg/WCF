ab8b62c58c85b40aaad5281fd8af9fa7d0703bc3
Brian <xan.brian@gmail.com>
2018-02-22 11:12:12 +0800

    up

--- Server/ServiceBase.cs
+++ Server/ServiceBase.cs
@@ -1,30 +1,46 @@
-﻿using Insight.Utils.Entity;
-
-namespace Insight.Utils.Server
+﻿namespace Insight.Utils.Server
 {
     public class ServiceBase
     {
-        protected string verifyUrl;
-
-        private Result<object> result = new Result<object>();
-        private string tokenId;
-        private string tenantId;
-        public string userName;
-        private string userId;
-        private string deptId;
+        /// <summary>
+        /// 租户ID
+        /// </summary>
+        public string tenantId { get; private set; }
 
         /// <summary>
-        /// 身份验证方法
+        /// 登录部门ID
+        /// </summary>
+        public string deptId { get; private set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public string userId { get; private set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string userName { get; private set; }
+
+        /// <summary>
+        /// 会话合法性验证
         /// </summary>
         /// <param name="key">操作权限代码，默认为空，即不进行鉴权</param>
+        /// <param name="id">用户ID</param>
         /// <returns>bool 身份是否通过验证</returns>
-        public bool Verify(string key = null)
+        public bool Verify(string key = null, string id = null)
         {
-            var verify = new Verify(verifyUrl, key);
-            userId = verify.Token.userId;
-            result = verify.result;
+            var verify = new Verify();
+            key = verify.userId == id ? null : key;
+            if (!verify.Compare(key)) return false;
 
-            return result.successful;
+            var session = verify.result.data;
+            tenantId = session.tenantId;
+            deptId = session.deptId;
+            userId = session.userId;
+            userName = session.userName;
+
+            return true;
         }
     }
 }
--- Server/Verify.cs
+++ Server/Verify.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.ServiceModel.Web;
 using Insight.Utils.Common;
 using Insight.Utils.Entity;
@@ -8,76 +7,47 @@ namespace Insight.Utils.Server
 {
     public class Verify
     {
-        private AccessToken accessToken;
-        private UriTemplateMatch uri;
+        private readonly string baseServer = Util.GetAppSetting("BaseServer");
+        private readonly string token;
+
+        // 验证结果
+        public Result<Session> result = new Result<Session>();
+
+        // 用户ID
+        public string userId;
 
         /// <summary>
-        /// 验证结果
+        /// 构造函数
         /// </summary>
-        public Result<object> result = new Result<object>();
-
-        /// <summary>
-        /// Access Token字符串
-        /// </summary>
-        public string token { get; private set; }
-
-        /// <summary>
-        /// Access Token对象
-        /// </summary>
-        public AccessToken Token
+        public Verify()
         {
-            get
-            {
-                if (accessToken != null) return accessToken;
+            var context = WebOperationContext.Current;
+            if (context == null) return;
 
-                accessToken = Util.Base64ToAccessToken(token);
-                return accessToken;
-            }
-        }
-
-        /// <summary>
-        /// 会话合法性验证
-        /// </summary>
-        /// <param name="verifyurl">验证服务URL</param>
-        /// <param name="key">操作权限代码，默认为空(不进行鉴权)</param>
-        public Verify(string verifyurl, string key = null)
-        {
-            if (!GetToken())
-            {
-                result.InvalidAuth();
-                return;
-            }
-
-            var url = $"{verifyurl}?action={key}";
-            var request = new HttpRequest(token);
-            if (!request.Send(url))
-            {
-                result.BadRequest(request.Message);
-                return;
-            }
-
-            result = Util.Deserialize<Result<object>>(request.Data);
+            var request = context.IncomingRequest;
+            var headers = request.Headers;
+            token = headers[HttpRequestHeader.Authorization];
+            userId = Util.Base64ToAccessToken(token)?.userId;
         }
 
         /// <summary>
         /// 获取Http请求头部承载的Access Token
         /// </summary>
-        /// <returns>boll Http请求头部是否承载Access Token</returns>
-        private bool GetToken()
+        /// <param name="key">操作权限代码，默认为空，即不进行鉴权</param>
+        /// <returns>boll 是否通过验证</returns>
+        public bool Compare(string key = null)
         {
-            var context = WebOperationContext.Current;
-            if (context == null) return false;
+            var url = $"{baseServer}/authapi/v1.0/tokens/verify?action={key}";
+            var request = new HttpRequest(token);
+            if (!request.Send(url))
+            {
+                result.BadRequest(request.Message);
+                return false;
+            }
 
-            var request = context.IncomingRequest;
-            uri = request.UriTemplateMatch;
+            result = Util.Deserialize<Result<Session>>(request.Data);
 
-            var headers = request.Headers;
-            token = headers[HttpRequestHeader.Authorization];
-            if (!string.IsNullOrEmpty(token)) return true;
-
-            var response = context.OutgoingResponse;
-            response.StatusCode = HttpStatusCode.Unauthorized;
-            return false;
+            return result.successful;
         }
     }
 }
