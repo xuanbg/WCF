52828f8bd4c95b2190b514c71eea0a81b6ce32e5
Brian <xan.brian@gmail.com>
2018-08-28 17:41:29 +0800

    增加分布式锁和业务编码生成器

---
+++ Redis/Generator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Insight.Utils.Redis
+{
+    public static class Generator
+    {
+        /// <summary>
+        /// 根据指定的格式生成业务编码
+        /// </summary>
+        /// <param name="format">编码格式</param>
+        /// <param name="group">分组格式</param>
+        /// <returns>业务编码</returns>
+        public static string newCode(string format, string group)
+        {
+            var index = format.IndexOf("#", StringComparison.Ordinal);
+            if (index < 0) return null;
+
+            format = replace(format);
+            group = replace(group);
+
+            var digits = format.Substring(index + 1, 1);
+            var key = $"CodeGroup:{group}#{digits}";
+            if (!LockHandler.getLock(group, 10)) return null;
+
+            var number = 1;
+            var val = RedisHelper.stringGet(key);
+            if (!string.IsNullOrEmpty(val)) number = Convert.ToInt32(val) + 1;
+
+            var len = Convert.ToInt32(digits);
+            var code = toString(number, len);
+
+            format = format.Replace($"#{len}", code);
+            RedisHelper.stringSet(key, number);
+            LockHandler.releaseLock(group);
+
+            return format;
+        }
+
+        /// <summary>
+        /// 日期格式替换为当前日期对应值
+        /// </summary>
+        /// <param name="str">输入字符串</param>
+        /// <returns>替换后字符串</returns>
+        private static string replace(string str)
+        {
+            var array = new[] {"yyyy", "yy", "MM", "dd"};
+            var now = DateTime.Now;
+            foreach (var format in array)
+            {
+                var date = now.ToString(format);
+                str = str.Replace(format, date);
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// 数字转制定位数的字符串，位数不足左补零，位数超出丢弃高位数字
+        /// </summary>
+        /// <param name="number">输入数字</param>
+        /// <param name="length">字符串位数</param>
+        /// <returns>左补零后的字符串</returns>
+        private static string toString(int number, int length)
+        {
+            var code = number.ToString($"D{length}");
+            var exceed = code.Length - length;
+            if (exceed > 0) code = code.Substring(exceed, length);
+
+            return code;
+        }
+    }
+}
---
+++ Redis/LockHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace Insight.Utils.Redis
+{
+    public static class LockHandler
+    {
+        /// <summary>
+        /// 获取分布式锁
+        /// </summary>
+        /// <param name="name">锁名称</param>
+        /// <param name="tryTime">重试时间(秒)，默认5秒</param>
+        /// <param name="expire">超时时间(秒)，默认值30秒</param>
+        /// <returns></returns>
+        public static bool getLock(string name, int tryTime = 5, int expire = 30)
+        {
+            if (string.IsNullOrEmpty(name)) return false;
+
+            var key = $"Lock:{name}";
+            var outTime = DateTime.Now.AddSeconds(tryTime);
+            while (true)
+            {
+                if (DateTime.Now > outTime) return false;
+
+                if (RedisHelper.hasKey(key))
+                {
+                    Thread.Sleep(100);
+                    continue;
+                }
+
+                RedisHelper.stringSet(key, key, new TimeSpan(0, 0, 0, expire));
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 释放锁
+        /// </summary>
+        /// <param name="name">锁名称</param>
+        public static void releaseLock(string name)
+        {
+            if (string.IsNullOrEmpty(name)) return;
+
+            var key = $"Lock:{name}";
+            RedisHelper.delete(key);
+        }
+    }
+}
